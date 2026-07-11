556d13753f85f4d44578bd3afbaeac0018b12b61
gkberkhayali <gokberkhayali@gmail.com>
2026-07-11 11:51:20 +0200

    Implemented pydantic.

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI
+from models import Product
 
 app = FastAPI()
 
+products = [
+    Product(id=1, name="Product 1", description="Description 1", price=10.99, quantity=100),
+    Product(id=2, name="Product 2", description="Description 2", price=20.99, quantity=50)
+]
+
 @app.get("/")
 def welcome():
-    return {"message": "Welcome to the application!"}
+    return {"message": "Welcome to the FASTAPI backend!"}
 
-@app.get("/Test")
-def welcomeTest():
-    return {"message": "Test Get request successful!"}
-
-@app.get("/Test/Test2")
-def welcomeTest2():
-    return {"message": "Test2 Get request successful!"}
+@app.get("/products")
+def get_all_products():
+   return products
