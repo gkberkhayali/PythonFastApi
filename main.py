69b9f5e738924c3d83f92d129803f562fa7c34c2
gkberkhayali <gokberkhayali@gmail.com>
2026-07-11 11:30:18 +0200

    Get method overloading on python for testing.,

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def welcome():
     return {"message": "Welcome to the application!"}
+
+@app.get("/Test")
+def welcomeTest():
+    return {"message": "Test Get request successful!"}
+
+@app.get("/Test/Test2")
+def welcomeTest2():
+    return {"message": "Test2 Get request successful!"}
